9702fa6189d789cfe38b6e9a736f53fc362360f8
Hamza Asghr <hamzaraftar765@gmail.com>
2024-08-12 06:04:10 -0700

    Designing Sticky Scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCourses from "./components/FeaturedCourses";
 import Hero from "./components/Hero";
+import WhyChoseUs from "./components/WhyChoseUs";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FeaturedCourses />
+      <WhyChoseUs />
     </>
   );
 }
