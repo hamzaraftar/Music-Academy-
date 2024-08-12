4da4e1e6b1573527e03b3b519572e7ec19b018bb
Hamza Asghr <hamzaraftar765@gmail.com>
2024-08-12 05:31:16 -0700

    featured Card section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from "./components/FeaturedCourses";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <FeaturedCourses />
     </>
   );
 }
